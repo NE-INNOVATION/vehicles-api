3497ee2148b9b5c03c2d270dbe614f8c963b99b0
Pratik <pratik1070061@gmail.com>
2020-05-25 11:07:25 -0400

    added health, liveness, readyness endpoints

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { route } = require('./api')
 const dataStore = require('./data/dataStore')
+const health = require('@cloudnative/health-connect')
+
+const healthcheck = new health.HealthChecker()
 
 module.exports = () => {
   
@@ -14,6 +17,10 @@ module.exports = () => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
+  
+  app.use('/live', health.LivenessEndpoint(healthcheck))
+  app.use('/ready', health.ReadinessEndpoint(healthcheck))
+  app.use('/health', health.HealthEndpoint(healthcheck))
   
   app.use(bodyParser.json())
   app.use('/api', route)
