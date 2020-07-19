b21b6fe7e7c126e3bdedfe9f95e98b4772c6a09a
Pratik <pratik1070061@gmail.com>
2020-07-19 09:37:23 -0400

    add try catch with logging

--- server/api/vehicles/routes.js
+++ server/api/vehicles/routes.js
@@ -4,6 +4,10 @@ const router = express.Router({ mergeParams: true });
 const dataStore = require("../../data/dataStore");
 const axios = require("axios");
 const { Agent } = require("https");
+const winston = require("winston");
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+});
 
 var gen = rn.generator({
   min: 100000000,
@@ -19,12 +23,16 @@ const client = axios.create({
 
 router
   .route("/vehicleInfo/:id/:quoteId")
-  .get(async (req, res, next) => {
+  .get(async (req, res) => {
+    logger.info(
+      `app.api.vehicles - getting vehicle with id - ${req.params.id}`
+    );
     res.send(
       JSON.stringify(await getVehicleInfo(req.params.id, req.params.quoteId))
     );
   })
-  .post(async (req, res, next) => {
+  .post(async (req, res) => {
+    logger.info(`app.api.vehicles - creating new vehicle`);
     res.send(
       JSON.stringify({
         result: await saveVehicleInfo(req.body, req.params.quoteId),
@@ -33,47 +41,53 @@ router
   });
 
 let getVehicleInfo = async (id, quoteId) => {
-  console.log("Returning Vehicle #", id);
-  let vehicle = await dataStore.findVehicle(quoteId);
-  return vehicle;
+  try {
+    let vehicle = await dataStore.findVehicle(quoteId);
+    return vehicle;
+  } catch (error) {
+    logger.error(
+      `app.api.vehicles - getting vehicle#${id}, from quote#${quoteId} failed - ${JSON.stringify(
+        error
+      )}`
+    );
+  }
 };
 
 let saveVehicleInfo = async (data, quoteId) => {
-  let vehicle = "";
-  // if(data.id !== ''){
-  //   vehicle = await dataStore.findVehicle(quoteId);
-  // }else{
-  vehicle = {};
-  vehicle.quoteId = quoteId;
-  // }
+  try {
+    let vehicle = "";
+    vehicle = {};
+    vehicle.quoteId = quoteId;
+    vehicle.year = data.year;
+    vehicle.make = data.make;
+    vehicle.model = data.model;
+    vehicle.vehicleOwned = data.vehicleOwned;
+    vehicle.vehicleUsage = data.vehicleUsage;
+    vehicle.daysDriven = data.daysDriven;
+    vehicle.milesDriven = data.milesDriven;
+    vehicle.vehiclePrimaryUse = data.vehiclePrimaryUse;
+    vehicle.annualMileage = data.annualMileage;
 
-  vehicle.year = data.year;
-  vehicle.make = data.make;
-  vehicle.model = data.model;
-  vehicle.vehicleOwned = data.vehicleOwned;
-  vehicle.vehicleUsage = data.vehicleUsage;
-  vehicle.daysDriven = data.daysDriven;
-  vehicle.milesDriven = data.milesDriven;
-  vehicle.vehiclePrimaryUse = data.vehiclePrimaryUse;
-  vehicle.annualMileage = data.annualMileage;
-
-  if (!data.id) {
-    vehicle.id = gen().toString();
-  }
-
-  await client.post(
-    `${process.env.DB_SERVICE_URL}/${process.env.COLLECTION_NAME}`,
-    vehicle,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
+    if (!data.id) {
+      vehicle.id = gen().toString();
     }
-  );
 
-  //dataStore.addVehicle(vehicle);
+    await client.post(
+      `${process.env.DB_SERVICE_URL}/${process.env.COLLECTION_NAME}`,
+      vehicle,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return vehicle.id;
 
-  return vehicle.id;
+  } catch (error) {
+    logger.error(
+      `app.api.vehicles - error creating new driver - ${JSON.stringify(error)}`
+    );
+  }
 };
 
 module.exports = router;
