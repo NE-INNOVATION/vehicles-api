f714c749fb6512ed10853e6ad1dfbf66cc547dfd
Pratik <pratik1070061@gmail.com>
2021-03-08 01:01:50 -0500

    adding vehicle changes using mongoose

--- server/api/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const vehicleRoutes = require('./vehicles').routes
-
-const router = express.Router({mergeParams: true})
-router.use('/vehicles', vehicleRoutes)
-
-module.exports = router
